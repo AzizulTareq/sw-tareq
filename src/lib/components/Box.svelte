4fa84eba3a0b38c4895ba9e83a674080f98910de
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-09-02 17:03:11 +0600

    all boxes converted to a single component

---
+++ src/lib/components/Box.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let backgroundColor = "";
+  export let rowColDetais = "";
+  export let hiddenInSmallDevice = false;
+  export let extraClass = "";
+</script>
+
+<div
+  class={`${backgroundColor} ${rowColDetais} ${
+    hiddenInSmallDevice && ` hidden md:block ${extraClass}`
+  }`}
+></div>
--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -1,17 +1,41 @@
+<script>
+  import Box from "../../lib/components/Box.svelte";
+</script>
+
 <div>
-  <div class="grid grid-rows-6 grid-cols-12 h-screen">
-    <div class="bg-blue-400 row-span-3 col-span-2"></div>
-    <div class="bg-teal-400 row-span-2 row-start-4 col-span-2"></div>
-    <div
-      class="bg-fuchsia-400 row-span-1 row-start-6 col-span-6 col-start-1"
-    ></div>
-
-    <div
-      class="bg-rose-100 row-span-5 row-start-1 col-span-4 col-start-3"
-    ></div>
-
-    <div class="bg-cyan-400 row-span-2 col-span-6"></div>
-    <div class="bg-yellow-100 row-span-4 col-span-4"></div>
-    <div class="bg-emerald-400 row-span-4 col-span-2"></div>
+  <div class="grid grid-rows-6 md:grid-cols-12 grid-cols-4 grid h-screen">
+    <Box
+      backgroundColor={"bg-blue-400"}
+      rowColDetais={`row-span-3 col-span-2`}
+      hiddenInSmallDevice={true}
+    />
+    <Box
+      backgroundColor={"bg-teal-400"}
+      rowColDetais={`row-span-2 col-span-2 row-start-4`}
+      hiddenInSmallDevice={true}
+    />
+    <Box
+      backgroundColor={"bg-fuchsia-400"}
+      rowColDetais={`row-span-1 col-span-6 row-start-6`}
+      hiddenInSmallDevice={true}
+    />
+    <Box
+      backgroundColor={"bg-rose-100"}
+      rowColDetais={`md:row-span-5 row-span-3 md:row-start-1 col-span-4 md:col-start-3`}
+    />
+    <Box
+      backgroundColor={"bg-cyan-400"}
+      rowColDetais={`row-span-2 col-span-6`}
+      hiddenInSmallDevice={true}
+    />
+    <Box
+      backgroundColor={"bg-yellow-100"}
+      rowColDetais={`row-span-2 md:row-span-4 row-span-3 col-span-4`}
+    />
+    <Box
+      backgroundColor={"bg-emerald-400"}
+      rowColDetais={`row-span-4 col-span-2`}
+      hiddenInSmallDevice={true}
+    />
   </div>
 </div>
