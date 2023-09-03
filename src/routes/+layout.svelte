02cd156ac180c410feda523f545c6dad73bedfdf
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-09-03 18:56:11 +0600

    home button implemented

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script>
   import "../app.css";
+  import { page } from "$app/stores";
 </script>
 
-<!-- <div>
-  {#if $page.url.pathname != '/'}
-  <a href="/">Home</a>
-  {/if}
-</div> -->
 <main>
+  <div>
+    {#if $page.url.pathname != "/"}
+      <a
+        class="fixed px-3 py-2 top-2 left-6 p-2 rounded bg-rose-600 text-sm text-white font-semibold shadow-lg z-10"
+        href="/">Home</a
+      >
+    {/if}
+  </div>
+
   <slot />
 </main>
